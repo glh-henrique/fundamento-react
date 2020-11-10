412e9d036819472e164d06a92bebb406e9896d4f
glh-henrique <glh.henrique@gmail.com>
2020-11-10 14:37:58 -0300

    criando primeiro componente react sem jsx

---
+++ src/components/basics/ComponentOne.js
@@ -0,0 +1,3 @@
+export default function ReturnString() {
+    return 'Primeiro Componente!';
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+//css
+import './index.css';
+
+//compnentes
+import ReturnString from './components/basics/ComponentOne';
+
+ReactDOM.render(
+    <div>
+        <ReturnString></ReturnString>
+    </div>,
+    document.getElementById('root')
+);
